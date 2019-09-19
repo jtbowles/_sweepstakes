8bd121fac3147edca46b8c913dcaa24569fb66ec
Jason Bowles <jbowles96@gmail.com>
2019-09-19 15:17:18 -0500

    UI.cs formatting and MVP

--- Sweepstakes/UI.cs
+++ Sweepstakes/UI.cs
@@ -11,16 +11,29 @@ namespace Sweepstakes
         public static string SelectSweepstakesType()
         {
             Console.Clear();
+            Console.WriteLine("  ---------------------------------------------------------------");
             Console.WriteLine("   Would you like to store your sweepstakes in a stack or queue?");
             Console.WriteLine("  ---------------------------------------------------------------");
-            Console.WriteLine("                  enter [stack] or [queue]");
-            string userInput = Console.ReadLine();
-            return userInput;
+            Console.WriteLine("                    enter [stack] or [queue]");
+            string userInput = Console.ReadLine().ToLower();
+            if (userInput == "stack")
+            {
+                return userInput;
+            }
+            else if (userInput == "queue")
+            {
+                return userInput;
+            }
+            else
+            {
+                return SelectSweepstakesType();
+            }
         }
 
         public static void DisplayPastSweepstakes(Sweepstakes sweepstakes)
         {
             Console.Clear();
+            Console.WriteLine("  -------------------------------------------------------------------");
             Console.WriteLine("   {0} sweepstakes  | Winner: {1} {2}", sweepstakes.name, sweepstakes.winningContestant.FirstName, sweepstakes.winningContestant.LastName);
             Console.WriteLine("  -------------------------------------------------------------------");
             Console.WriteLine("                     press [enter] to continue");
@@ -30,14 +43,17 @@ namespace Sweepstakes
         public static void DisplaySweepstakesType(ISweepstakesManager manager)
         {
             Console.Clear();
+            Console.WriteLine("  --------------------------------------------------------------");
             Console.WriteLine("   Currently using a {0} database to store our sweepstakes", manager.GetName());
-            Console.WriteLine("  ------------------------------------------------------------");
-            Console.WriteLine("                  press [enter] to continue");
+            Console.WriteLine("  --------------------------------------------------------------");
+            Console.WriteLine("                   press [enter] to continue");
+            Console.ReadLine();
         }
 
         public static void DisplayNoSweepstakesStored()
         {
             Console.Clear();
+            Console.WriteLine("  --------------------------------------");
             Console.WriteLine("     No past sweepstakes to display!");
             Console.WriteLine("   Must create a new sweepstakes first!");
             Console.WriteLine("  --------------------------------------");
@@ -48,11 +64,13 @@ namespace Sweepstakes
         public static int DisplayMarketingFirmMenu()
         {
             Console.Clear();
+            Console.WriteLine("  -------------------------------------");
             Console.WriteLine("   Welcome to the Marketing Firm Menu");
-            Console.WriteLine("  --------------------------------------");
+            Console.WriteLine("  -------------------------------------");
             Console.WriteLine("   enter [1] to create a new Sweepstakes");
             Console.WriteLine("   enter [2] to view a past sweepstakes");
             Console.WriteLine("   enter [3] to quit");
+            Console.WriteLine("  -------------------------------------");
             int userInput = Convert.ToInt32(Console.ReadLine());
             return userInput;
         }
@@ -60,12 +78,14 @@ namespace Sweepstakes
         public static int DisplaySweepstakesMenu(string name)
         {
             Console.Clear();
+            Console.WriteLine("  ----------------------------------------");
             Console.WriteLine("   Welcome to the {0} sweepstakes", name);
-            Console.WriteLine("  --------------------------------------");
+            Console.WriteLine("  ----------------------------------------");
             Console.WriteLine("   enter [1] check the number of registered contestants");
             Console.WriteLine("   enter [2] change the number of contestants to register");
             Console.WriteLine("   enter [3] generate contestants");
             Console.WriteLine("   enter [4] to pick a winner");
+            Console.WriteLine("  ----------------------------------------");
             int userInput = Convert.ToInt32(Console.ReadLine());
             return userInput;
         }
@@ -73,25 +93,21 @@ namespace Sweepstakes
         public static bool GetNotificationResponse()
         {
             Console.Clear();
-            Console.WriteLine("   Would you like to notify the participants of the Winner?");
-            Console.WriteLine("  ---------------------------------------------------------");
-            Console.WriteLine("                 enter [yes] or [no]");
-            try
+            Console.WriteLine("  ----------------------------------------------------------");
+            Console.WriteLine("   Would you like to notify the participants of the winner?");
+            Console.WriteLine("  ----------------------------------------------------------");
+            Console.WriteLine("                   enter [yes] or [no]");
+            string userInput = Console.ReadLine().ToLower();
+            if(userInput == "yes")
             {
-                string userInput = Console.ReadLine().ToLower();
-                if(userInput == "yes")
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-
+                return true;
             }
-            catch (Exception e)
+            else if (userInput == "no")
             {
-                Console.WriteLine(e.Message);
+                return false;
+            }
+            else
+            {
                 return GetNotificationResponse();
             }
         }
@@ -99,6 +115,7 @@ namespace Sweepstakes
         public static void DisplayCurrentContestantsToRegister(int numberOfContestantsToGenerate)
         {
             Console.Clear();
+            Console.WriteLine("  --------------------------------------------");
             Console.WriteLine("   New number of contestants to register: {0}", numberOfContestantsToGenerate);
             Console.WriteLine("  --------------------------------------------");
             Console.WriteLine("         press [enter] to continue");
@@ -108,6 +125,7 @@ namespace Sweepstakes
         public static int ChangeNumberOfContestantsToGenerate()
         {
             Console.Clear();
+            Console.WriteLine("  --------------------------------------------------------");
             Console.WriteLine("   enter the [number] of contestants you want to generate");
             Console.WriteLine("  --------------------------------------------------------");
             int userInput = Convert.ToInt32(Console.ReadLine());
@@ -117,6 +135,7 @@ namespace Sweepstakes
         public static void DisplayContestantsNotGenerated()
         {
             Console.Clear();
+            Console.WriteLine("  ----------------------------------------------------");
             Console.WriteLine("   Must register contestants before picking a winner!");
             Console.WriteLine("  ----------------------------------------------------");
             Console.WriteLine("               press [enter] to return");
@@ -126,24 +145,19 @@ namespace Sweepstakes
         public static void DisplayWinnerOfSweepstakes(string name, Contestant winner)
         {
             Console.Clear();
+            Console.WriteLine("  ----------------------------------------");
             Console.WriteLine("   Winner of the {0} sweepstakes", name);
-            Console.WriteLine("  --------------------------------------");
+            Console.WriteLine("  ----------------------------------------");
             Console.WriteLine("   Congratulations to {0} {1}!", winner.FirstName, winner.LastName);
-            Console.ReadLine();
-        }
-
-        public static void DisplayWinnerNotPicked()
-        {
-            Console.Clear();
-            Console.WriteLine("   Winner must be picked prior to notifying contestants!");
-            Console.WriteLine("  -------------------------------------------------------");
-            Console.WriteLine("                 press [enter] to return");
+            Console.WriteLine("  ----------------------------------------");
+            Console.WriteLine("        press [enter] to continue");
             Console.ReadLine();
         }
 
         public static void DisplayNumberOfRegisteredContestants(int numOfContestants)
         {
             Console.Clear();
+            Console.WriteLine("  ----------------------------------------------");
             Console.WriteLine("   Total number of registered contestants:  {0}", numOfContestants);
             Console.WriteLine("  ----------------------------------------------");
             Console.ReadLine();
@@ -152,10 +166,12 @@ namespace Sweepstakes
         public static string SetNumberOfContestantsToGenerate(int numberOfContestantsToGenerate)
         {
             Console.Clear();
+            Console.WriteLine("  -----------------------------------------");
             Console.WriteLine("   Number of contestants to register: {0}", numberOfContestantsToGenerate);
             Console.WriteLine("  -----------------------------------------");
             Console.WriteLine("    would you like to change this number?");
             Console.WriteLine("           enter [yes] or [no]");
+            Console.WriteLine("  -----------------------------------------");
             string userInput = Console.ReadLine();
             return userInput;
         }
@@ -163,11 +179,13 @@ namespace Sweepstakes
         public static void DisplayRegisteredContestantInfo(Contestant contestant)
         {
             Console.Clear();
+            Console.WriteLine("  ------------------------------------");
             Console.WriteLine("   Registered Contestant # {0} ", contestant.RegistrationNumber);
             Console.WriteLine("  ------------------------------------");
             Console.WriteLine("   Full name: {0} {1}", contestant.FirstName, contestant.LastName);
             Console.WriteLine("   Email: {0}", contestant.EmailAddress);
             Console.WriteLine("  ------------------------------------");
+            Console.WriteLine("      press [enter] to continue");
             Console.ReadLine();
 
         }
@@ -175,6 +193,7 @@ namespace Sweepstakes
         public static string SetNameOfSweepstakes()
         {
             Console.Clear();
+            Console.WriteLine("  ------------------------------------");
             Console.WriteLine("   Enter the name of this Sweepstakes: ");
             Console.WriteLine("  ------------------------------------");
             string userInput = Console.ReadLine();
@@ -184,6 +203,7 @@ namespace Sweepstakes
         public static string SetContestantFirstName()
         {
             Console.Clear();
+            Console.WriteLine("  ------------------------------------");
             Console.WriteLine("   Enter the contestant's first name: ");
             Console.WriteLine("  ------------------------------------");
             string userInput = Console.ReadLine();
@@ -193,6 +213,7 @@ namespace Sweepstakes
         public static string SetContestantLastName()
         {
             Console.Clear();
+            Console.WriteLine("  ------------------------------------");
             Console.WriteLine("   Enter the contestant's last name: ");
             Console.WriteLine("  ------------------------------------");
             string userInput = Console.ReadLine();
@@ -202,6 +223,7 @@ namespace Sweepstakes
         public static string SetContestantEmailAddress()
         {
             Console.Clear();
+            Console.WriteLine("  ---------------------------------------");
             Console.WriteLine("   Enter the contestant's email address: ");
             Console.WriteLine("  ---------------------------------------");
             string userInput = Console.ReadLine();
